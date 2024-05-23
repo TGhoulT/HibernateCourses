9e7da9d7516984829da1a7ad866204239602a2d1
Malis_pv <mgenius012975@gmail.com>
2024-05-23 16:25:02 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,27 @@
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        StandardServiceRegistry standardServiceRegistry = new StandardServiceRegistryBuilder()
+                .configure("hibernate.cfg.xml").build();
+        Metadata metadata = new MetadataSources(standardServiceRegistry).getMetadataBuilder().build();
+        SessionFactory sessionFactory = metadata.getSessionFactoryBuilder().build();
+
+        Session session = sessionFactory.openSession();
+        Course course = session.get(Course.class, 3);
+        List<Student> students = course.getStudents();
+        /*System.out.println("Course:" + course);*/
+        Teacher teacher = course.getTeacher();
+        System.out.println(teacher);
+        students.forEach(System.out::println);
+
+    }
+
+}
